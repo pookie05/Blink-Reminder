7b79d9104cc9a670a6d688f416a517cff051ba74
Apurva kisaan <apurvakisaan@gmail.com>
2026-01-17 13:05:38 +0530

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -2,7 +2,7 @@ import time
 
 
 class BlinkReminder:
-    def __init__(self, remind_after=10, cooldown=10, overlay_duration=3):
+    def __init__(self, remind_after=10, cooldown=10, overlay_duration=5):
         self.remind_after = remind_after
         self.cooldown = cooldown
         self.overlay_duration = overlay_duration
